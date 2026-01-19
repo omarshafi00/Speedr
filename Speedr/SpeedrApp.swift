a3d815e7d567458c4cd7c590139b19afb83d3be6
Omar Alshafi <omar.alshafi020@gmail.com>
2026-01-19 08:48:53 +0100

    Initial commit - Xcode project setup

---
+++ Speedr/SpeedrApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedrApp.swift
+//  Speedr
+//
+//  Created by user291679 on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct SpeedrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpeedrTests/SpeedrTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedrTests.swift
+//  SpeedrTests
+//
+//  Created by user291679 on 1/19/26.
+//
+
+import Testing
+@testable import Speedr
+
+struct SpeedrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
